3e9df10f7ea71663b746a7fa93364964f1bed3e3
Lars Hupfeldt <lhn@hupfeldtit.dk>
2023-12-10 23:41:34 +0100

    Fix version test.

--- test/version_test.py
+++ test/version_test.py
@@ -4,4 +4,4 @@ import file_groups
 
 
 def test_version_of_properly_installe_package():
-    assert re.match(r"[0-9]+\.[0-9]+\.[0-9]+\..*", file_groups.__version__)
+    assert re.match(r"[0-9]+\.[0-9]+\.[0-9]+.*", file_groups.__version__)
